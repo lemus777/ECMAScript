f1cbe585294684ac3706798b332aa640606b3ddf
Lemus <lemus777@gmail.com>
2022-08-06 20:15:33 +0200

    Primer archivo js y json

---
+++ src/es6/index.js
@@ -0,0 +1,23 @@
+// Declaración de valores por defecto antes
+function newFunction(name, age, country) {
+    var name = name || 'Oscar';
+    var age = age || 32;
+    var country = country || 'MX';
+    console.log(name, age, country);
+}
+
+// es6
+function newFunction2(name = 'Oscar', age = 32, country = 'MX') {
+    console.log(name, age, country);
+}
+
+newFunction2();
+newFunction2('Ricardo', 32, 'CO');
+
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
